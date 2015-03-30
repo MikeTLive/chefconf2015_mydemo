54f11e97916080095ab4dd90ac7ac83e03f3837e
mthibodeau1 <Michael_Thibodeau@intuit.com>
2015-03-29 19:39:36 -0700

    updated metadata and Berksfile for INTUchef

--- Berksfile
+++ Berksfile
@@ -1,3 +1,3 @@
-source "https://supermarket.chef.io"
+source "https://ec2-52-4-136-60.compute-1.amazonaws.com"
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description      'Installs/Configures chefconf2015_mydemo'
 long_description 'Installs/Configures chefconf2015_mydemo'
 version          '0.1.0'
 
+source_url "https://github.com/chefconf2015/chefconf2015_mydemo"
+issues_url "https://github.com/chefconf2015/chefconf2015_mydemo/issues"
